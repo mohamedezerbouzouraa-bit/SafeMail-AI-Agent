4a8fef3f956e374dca8aa3136a4327032a043fe1
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-21 23:50:40 +0100

    Implement session creation in session_service.py

---
+++ services/session_service.py
@@ -0,0 +1,14 @@
+from google.adk.sessions import InMemorySessionService
+
+APP_NAME = "email_agent_app"
+USER_ID = "user_001"
+SESSION_ID = "email_session"
+
+session_service = InMemorySessionService()
+
+async def create_session():
+    await session_service.create_session(
+        app_name=APP_NAME,
+        user_id=USER_ID,
+        session_id=SESSION_ID
+    )
